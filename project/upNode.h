d238819d78a666f6d64cc96784f1f2fc9e0a1520
Ghulam Habib <ghulamhabib.x@gmail.com>
2026-01-03 01:46:24 +0500

    i am commit it because this is benefit of object oreinet programing you have to make two construtat with diff arg now i am update constaur and overload method for id2 to update it i leave this hear still my code work due to oop and method overloading

--- project/upNode.h
+++ project/upNode.h
@@ -21,11 +21,18 @@ public:
         this->table = table;
         this->next = NULL;
     }
-    
+    upNode(int id, int id2, const string &table)
+    {
+        this->id = id;
+        this->id2 = id2;
+        this->table = table;
+        this->next = NULL;
+    }
 
     upNode()
     {
         this->id = -1;
+        this->id2 = -1;
         this->table = "NULL";
         this->next = NULL;
     }
@@ -35,11 +42,23 @@ public:
         return next;
     }
 
-    pair<int, string> getMetaData() const
+    pair<pair<int, int>, string> getMetaData() const
     {
-        return {id, table};
+        return {{id, id2},   table};
+    }
+    int getId1()const
+    {
+        return id;
     }
 
+    int getId2()const
+    {
+        return id2;
+    }
+    string & getTable()
+    {
+        return table;
+    }
     void setNext(upNode *n)
     {
         next = n;
@@ -48,6 +67,12 @@ public:
     void setMetaData(int id, const string &table)
     {
         this->id = id;
+        this->table = table;
+    }
+    void setMetaData(int id, int id2, const string &table)
+    {
+        this->id = id;
+        this->id2 = id2;
         this->table = table;
     }
 };
