3b67526252b1e56689dcff9a7af3d028c7591035
ghulamhabib364 <ghulamhabib.x@gmail.com>
2026-01-03 23:59:47 +0500

    work on main to project workd smooth

--- project/main.cpp
+++ project/main.cpp
@@ -1,7 +1,6 @@
 // main.cpp - CORRECT ORDER
 #define _HAS_STD_BYTE 0 // Define it first
 #include <windows.h>    // Include Windows headers second
-
 // Now include C++ standard and your project headers
 #include <iostream>
 #include "sm_systen.h"
